6856c0a95836890779e65a79de5cc964b20cae2c
Giovananevess <giovana_neves2001@outlook.com>
2024-12-02 22:23:15 -0300

    last version 02-12

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -6,6 +6,21 @@ $(document).ready(function () {
   setupWhatsAppLinks();
 });
 
+function toggleAnswer(element) {
+  const answer = element.nextElementSibling;
+  const icon = element.querySelector("i");
+
+  if (answer.style.display === "block") {
+    answer.style.display = "none";
+    icon.classList.remove("fa-minus");
+    icon.classList.add("fa-plus");
+  } else {
+    answer.style.display = "block";
+    icon.classList.remove("fa-plus");
+    icon.classList.add("fa-minus");
+  }
+}
+
 // Gerenciar Menu Mobile
 function handleMobileMenu() {
   $("#mobile_btn").on("click", function () {
